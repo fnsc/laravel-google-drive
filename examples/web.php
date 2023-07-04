3bb1223c9d21b5fb60d9bcc7c3c2bb0f678bf9aa
Gabriel Fonseca <gabrieldfnsc@gmail.com>
2023-07-03 20:31:30 -0700

    feat(uploadMany): add upload many feature

--- examples/web.php
+++ examples/web.php
@@ -1,11 +1,18 @@
 <?php
 
+/**
+ * Examples
+ */
+
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use LaravelGoogleDrive\GoogleDrive;
 
+/**
+ * Upload single file example
+ */
 Route::post(
     '/',
     function (Request $request, GoogleDrive $service): JsonResponse {
@@ -19,6 +26,31 @@ Route::post(
     }
 );
 
+/**
+ * Upload many example
+ */
+Route::post(
+    '/upload-many',
+    function (Request $request, GoogleDrive $service): JsonResponse {
+        $uploadedFiles = $request->file('files');
+        $result = $service->uploadMany($uploadedFiles);
+
+        $payload = [];
+
+        foreach ($result as $fileData) {
+            $payload[] = [
+                'folder_id' => $fileData->getFolderId(),
+                'file_id' => $fileData->getFileId(),
+            ];
+        }
+
+        return new JsonResponse($payload);
+    }
+);
+
+/**
+ * Download single file example
+ */
 Route::get('/download', function (GoogleDrive $service): Response {
     $file = $service->get('file.txt', '1ch20VwBUDffhnD3qzr0_DZk4Dk5pOKO321');
 
